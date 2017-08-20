1b26dab6b4a2acca2b935a586117a910162b86c1
ubalderas <usbaldo.balderas@gmail.com>
2017-08-19 20:29:02 -0500

    Added validation library with results Recording and modified Test file using a test Case JSON object

--- test/SampleRequestTests.js
+++ test/SampleRequestTests.js
@@ -3,19 +3,31 @@
  */
 "use strict";
 const requestUtilities = require('../lib/httpRequestUtilities');
+const validationLib = require('../lib/validationLibrary');
 const assert = require('assert');
+const testEndpointURL = "https://jsonplaceholder.typicode.com/";
+const testService = 'posts';
 
-describe("Sample Project", function() {
+describe("Test Suite for the '" + testService + "' service", function() {
     this.timeout(5000);
     this.slow(1000);
     let contentType = "application/json";
-    let completeEndpointUrl = "https://jsonplaceholder.typicode.com/posts";
+    let completeEndpointUrl = testEndpointURL + testService;
 
-    it("Sends a single requests to posts and prints the response", function(done) {
+    let testCaseObject = {
+        "testURL": completeEndpointUrl,
+        "testNumber": "POSTS-001",
+        "testDescription": "Example 1: Should return a list of posts from jsonplaceholder",
+        "tags": ["P1", "POSTS"]
+    };
+
+    it(testCaseObject.testDescription, function(done) {
         requestUtilities.getRequest(completeEndpointUrl)
             .then( response => {
+                
                 assert.notEqual(response.body, undefined, "No response was received.");
-                console.log(response.body);
+                testCaseObject["actualResponse"] = response.body;
+                validationLib.recordResults(testCaseObject, './test/results', testCaseObject.testNumber);
                 done();
             })
             .catch(done);
